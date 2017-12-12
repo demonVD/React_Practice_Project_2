b63de034724309908dabb1529967631d7d831d5c
demonVD <dman.vis94@gmail.com>
2017-12-12 17:44:01 +0530

    added video player

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_item_list';
+import VideoDetails from './components/video_details';
 //api key for youtube
 const API_KEY='AIzaSyBmZM8VcBK2UV-CoOk4bhAeIfhtNvscWBQ';
 
@@ -13,7 +14,7 @@ class App extends Component{
         this.state={
             videos:[]
         }
-        YTSearch({key:API_KEY,term:'Vishant'},(data)=>{
+        YTSearch({key:API_KEY,term:'foo fighters'},(data)=>{
             this.setState({videos:data});
             console.log(data);
         })
@@ -22,8 +23,11 @@ class App extends Component{
     render(){
         return(
             <div>
-                <SearchBar/>
-                <VideoList videoList={this.state.videos}/>
+                <div><SearchBar/></div>
+                <div><VideoDetails video={this.state.videos[0]}/></div>
+                <div><VideoList videoList={this.state.videos}/></div>
+                
+                
             </div>
             
         );
