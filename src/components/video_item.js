e92563906a4854689971a823a8a0f4f2223069ac
demonVD <dman.vis94@gmail.com>
2017-12-12 16:34:52 +0530

    added bootstrap and layout for video list

--- src/components/video_item.js
+++ src/components/video_item.js
@@ -1,5 +1,20 @@
 import React from 'react';
 const VideoListItem=(props)=>{
-    return(<li>{props.video.etag}</li>);
+const video=props.video;
+const imageUrl=video.snippet.thumbnails.default.url;
+    return(
+    <div>
+        <li className="list-group-item">
+        <div className="video-list-media">
+            <div className="media-left">
+                <img className="media-object" src={imageUrl}/>
+            </div>
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </div>
+        </li>
+    </div>
+    );
 }
 export default VideoListItem;
